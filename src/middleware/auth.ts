527d8a299c161e3a68a8f709d07327f8def3fed4
DoMAAA21 <clashecurry21@gmail.com>
2024-03-29 08:03:29 +0800

    fix: auth middlware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,34 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
-import type { User } from '../models/userSchema';
 import jwt from 'jsonwebtoken';
 import db from '../database/db';
 import users from '../models/userSchema';
+import { eq } from 'drizzle-orm';
 
-interface CustomRequest extends Request {
-    ...Request,
-    user?: any; 
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    fname: string;
+    lname: string;
+    number: string;
+    active: boolean;
+    created_at: Date | string;
+    updated_at: Date | string;
 }
 
+interface CustomRequest extends Request {
+    user?: User;
+}
 
-export const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const isAuthenticated = async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
     const { token } = req.cookies;
-
     if (!token) {
         res.status(401).json({ error: "Login first to access this resource." });
-        return; 
+        return;
     }
-
     try {
         if (!process.env.JWT_SECRET) {
             res.status(500).json({ error: "Provide JWT SECRET FIRST" });
-            return; 
+            return;
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as { id : number};
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as { id: number };
+        const user = await db.select().from(users).where(eq(users.id, decoded.id));
 
-        req.user = await db.select().from(users).where('id', decoded).first();
-
-        next(); 
+        if (!user.length) {
+            res.status(500).json({ error: "User not found" });
+        }
+        req.user = user[0];
+        next();
     } catch (error: unknown) {
         if (error instanceof Error) {
             console.error(`Error in isAuthenticated middleware: ${error.message}`);
