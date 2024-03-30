3525dc6e7a71a34c9f6f07401cd1a7231dd5f484
DOMAA21 <clashecurry21@gmail.com>
2024-03-30 12:03:24 +0800

    feat: add validations

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
-import db from "../database/db";
-import users from "../models/userSchema";
+import db from '../database/db';
+import users from '../models/userSchema';
 import { eq } from 'drizzle-orm';
-
+import bcrypt from 'bcrypt';
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
         const allUsers = await db.select().from(users);
@@ -14,18 +14,36 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
 }
 
 
+
+
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
     try {
-        const { email } = req.body;
-        const existingUser = await db.select().from(users).where(eq(users.email, email));
+        const { email, username, number, password } = req.body;
 
-        if (existingUser.length >= 1) {
-            res.status(400).json({ error: 'User with this email already exists' });
+        const numberPattern = /^0\d{10}(?:\*{6})?$/;
+        if (!numberPattern.test(number)) {
+            res.status(400).json({ message: 'Invalid number format' });
             return;
         }
 
-        const newUser = await db.insert(users).values(req.body).returning();
-        console.log(newUser);
+        const existingUser = await db.select().from(users).where(eq(users.email, email));
+        if (existingUser.length >= 1) {
+            res.status(400).json({ message: 'User with this email already exists' });
+            return;
+        }
+        const duplicateUsername = await db.select().from(users).where(eq(users.username, username));
+        if (duplicateUsername.length >= 1) {
+            res.status(400).json({ message: 'Username is already taken' });
+            return;
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUserData ={
+            ...req.body,
+            password: hashedPassword
+        }
+        const newUser = await db.insert(users).values(newUserData).returning();
         res.json({ success: true, user: newUser });
     } catch (error) {
         if (error instanceof Error) {
